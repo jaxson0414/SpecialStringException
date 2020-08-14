78de545b7f2691b3c8822ef517071bac1053e149
Low Jun Jie <69612801+jaxson0414@users.noreply.github.com>
2020-08-14 10:25:13 +0800

    Add files via upload

---
+++ Special String Exception/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("\"Quotes\"");
+        System.out.println("Slashes \\//");
+        System.out.println("How '\"confounding' \"\\\" it is!");
+    }
+}
